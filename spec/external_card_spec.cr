3ccd6e21d9d24201bac7b869285b3eabfb655e23
Jeremy Woertink <jeremywoertink@gmail.com>
2024-07-08 15:02:40 -0700

    don't add thumb to external cards unless there's actually a thumb. Fixes #2

---
+++ spec/external_card_spec.cr
@@ -0,0 +1,22 @@
+require "./spec_helper"
+
+describe Bsky::ExternalCard do
+  context "with an image" do
+    it "adds the thumb key" do
+      img = Bsky::Image.new("ref:123", "image/jpg", 100)
+      card = Bsky::ExternalCard.new("www.site.com", "My Site", "Welcome to my site", img)
+
+      card.to_h["$type"].should eq("app.bsky.embed.external")
+      card.to_h.dig("external", "thumb", "ref", "$link").should eq("ref:123")
+    end
+  end
+
+  context "without an image" do
+    it "skips the thumb key" do
+      card = Bsky::ExternalCard.new("www.site.com", "My Site", "Welcome to my site")
+
+      card.to_h["$type"].should eq("app.bsky.embed.external")
+      card.to_h["external"].as(Hash).has_key?("thumb").should eq(false)
+    end
+  end
+end
